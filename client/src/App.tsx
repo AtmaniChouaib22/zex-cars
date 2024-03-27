db0a7c7bd7830d1b09895d01573e7f8dff03cdb0
Atmani Chouaib <atmanichouaib22@gmail.com>
2024-03-27 14:15:06 +0000

    basic routing/context setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,38 @@
+import { useState, createContext } from "react";
+import Navbar from "../src/components/custom/Navbar";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export const appContext = createContext({
+  isLogged: false,
+  setIsLogged: (isLogged: boolean) => {},
+  isLoading: false,
+  setIsLoading: (isLoading: boolean) => {},
+});
+
 function App() {
-  return <>hello world</>;
+  const [isLoagged, setIsLogged] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <appContext.Provider
+      value={{
+        isLogged: isLoagged,
+        setIsLogged: setIsLogged,
+        isLoading: isLoading,
+        setIsLoading: setIsLoading,
+      }}
+    >
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<div>About</div>} />
+          <Route path="/contact" element={<div>Contact</div>} />
+        </Routes>
+      </BrowserRouter>
+    </appContext.Provider>
+  );
 }
 
 export default App;
