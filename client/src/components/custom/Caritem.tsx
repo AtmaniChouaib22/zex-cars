49841c74d25b3993c90f09fc3e9ef0ba3c8f9399
Atmani Chouaib <atmanichouaib22@gmail.com>
2024-03-31 01:01:56 +0000

    Refactor Caritem component to use props

--- client/src/components/custom/Caritem.tsx
+++ client/src/components/custom/Caritem.tsx
@@ -1,24 +1,34 @@
 import { Link } from "react-router-dom";
 import { Badge } from "@/components/ui/badge";
 
-const Caritem = () => {
+const Caritem = ({
+  title,
+  id,
+  status,
+  price,
+  mileage,
+  location,
+  fuel_type,
+  image,
+}) => {
   return (
-    <Link to={`#`}>
+    <Link to={id}>
       <div className=" flex flex-col sm:h-72 sm:w-60 w-44 h-64 overflow-hidden shadow-md hover:shadow-zinc-800 rounded-md  bg-white transition duration-150 ease-out hover:ease-in hover:scale-105">
         <img
           className="h-auto w-full object-cover rounded-t-md"
-          src="../src/assets/tesla.jpg"
+          src={`../uploads/cars/${image}`}
         />
         <div className="p-2 flex flex-col gap-2 items-start">
-          <div className="text-lg font-bold truncate">title</div>
+          <div className="text-lg font-bold truncate">{title}</div>
           <div className="flex gap-1 items-center flex-wrap">
-            <Badge variant={"secondary"}>milage</Badge>
-            <Badge variant={"secondary"}>location</Badge>
-            <Badge variant={"secondary"}>fuel type</Badge>
+            <Badge variant={"secondary"}>{mileage}</Badge>
+            <Badge variant={"secondary"}>{location}</Badge>
+            <Badge variant={"secondary"}>{fuel_type}</Badge>
           </div>
         </div>
-        <div className="flex flex-col ">
-          <div className="p-2">price</div>
+        <div className="flex sm:flex-col pl-2 border-t-2 border-zinc-950">
+          <div className="p-2">{price}</div>
+          <div>{status}</div>
         </div>
       </div>
     </Link>
