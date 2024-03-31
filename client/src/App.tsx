61b9e060e5adb6e3cb833ce1afa2ab32fc6bb18f
Atmani Chouaib <atmanichouaib22@gmail.com>
2024-03-31 01:02:54 +0000

    Add new pages and state variables to App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,17 +3,30 @@ import Navbar from "../src/components/custom/Navbar";
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sellpage from "./pages/Sellpage";
+import BuyPage from "./pages/BuyPage";
+import LoginPage from "./pages/LoginPage";
 
 export const appContext = createContext({
   isLogged: false,
   setIsLogged: (isLogged: boolean) => {},
   isLoading: false,
   setIsLoading: (isLoading: boolean) => {},
+  error: null,
+  setError: (error: any) => {},
+  user: {},
+  setUser: (user: object) => {},
 });
 
 function App() {
   const [isLoagged, setIsLogged] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [user, setUser] = useState({
+    fullName: "",
+    admin: false,
+    email: "",
+    id: "",
+  });
 
   return (
     <appContext.Provider
@@ -22,6 +35,10 @@ function App() {
         setIsLogged: setIsLogged,
         isLoading: isLoading,
         setIsLoading: setIsLoading,
+        error: error,
+        setError: setError,
+        user: user,
+        setUser: setUser,
       }}
     >
       <BrowserRouter>
@@ -31,6 +48,9 @@ function App() {
           <Route path="/about" element={<div>About</div>} />
           <Route path="/contact" element={<div>Contact</div>} />
           <Route path="/sellcar" element={<Sellpage />} />
+          <Route path="/buycar" element={<BuyPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/profile" element={<div>profile</div>} />
         </Routes>
       </BrowserRouter>
     </appContext.Provider>
