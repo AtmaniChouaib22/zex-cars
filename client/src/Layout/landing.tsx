ffe58b8c94231b4df255c0be57f68ab7914487d0
abdelileh <abdelileh@gmail.com>
2024-03-31 08:55:08 +0200

    add a landing page and footer

---
+++ client/src/Layout/landing.tsx
@@ -0,0 +1,248 @@
+// Start Landing
+<div className="banner">
+  <div className="slider1 active">
+    <img src="../assets/img1.jpg" id="slid" alt="" />
+  </div>
+  <div className="slider2">
+    <img src="../assets/img2.jpg" id="slid" alt="" />
+  </div>
+  <div className="slider3">
+    <img src="../assets/img3.jpg" id="slid" alt="" />
+  </div>
+  <div className="overlay">
+    <div className="content">
+      <h1>Click your next</h1>
+      <p>Quick. Easy. Simple. Sell your car today with immediate payment.</p>
+      <div>
+        <a id="sell" href="#">
+          <span>SELL MY CAR</span>
+          <i className="fa fa-play spacer-ml-8"></i>
+        </a>
+        <a id="buy" href="#">
+          <span>BUY A CAR</span>
+          <i className="fa fa-play spacer-ml-8"></i>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>;
+// End Landing
+
+//  Start Services
+<div className="services" id="services">
+  <div className="container">
+    <div className="main-heading">
+      <h2>See Our</h2>
+      <p>Popular Service</p>
+    </div>
+    <div className="service-content">
+      <div className="serv-box">
+        <i className="fa-solid fa-location-arrow"></i>
+        <div className="text">
+          <h3>City Transfer</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut iure
+            deleniti quo iusto quas
+          </p>
+        </div>
+      </div>
+      <div className="serv-box">
+        <i className="fa-solid fa-building"></i>
+        <div className="text">
+          <h3>Whole City Tour</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut iure
+            deleniti quo iusto quas
+          </p>
+        </div>
+      </div>
+      <div className="serv-box">
+        <i className="fa-solid fa-car"></i>
+        <div className="text">
+          <h3>Unlimited Miles Car Rental</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut iure
+            deleniti quo iusto quas
+          </p>
+        </div>
+      </div>
+      <div className="serv-box">
+        <i className="fa-regular fa-clock"></i>
+        <div className="text">
+          <h3>Fast & Easy Booking</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut iure
+            deleniti quo iusto quas
+          </p>
+        </div>
+      </div>
+      <div className="serv-box">
+        <i className="fa-solid fa-plane-departure"></i>
+        <div className="text">
+          <h3>Airport Transfer</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut iure
+            deleniti quo iusto quas
+          </p>
+        </div>
+      </div>
+      <div className="serv-box">
+        <i className="fa-solid fa-location-dot"></i>
+        <div className="text">
+          <h3>Many Pickup Location</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut iure
+            deleniti quo iusto quas
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>;
+//  End Services
+
+//  Start footer
+<footer>
+  <div className="container">
+    <ul className="social">
+      <li>
+        <a href="#" className="facebook">
+          <i className="fab fa-facebook-f"></i>
+        </a>
+      </li>
+      <li>
+        <a href="#" className="twitter">
+          <i className="fab fa-twitter"></i>
+        </a>
+      </li>
+      <li>
+        <a href="#" className="youtube">
+          <i className="fab fa-youtube"></i>
+        </a>
+      </li>
+      <li>
+        <a href="#" className="instagram">
+          <i className="fa-brands fa-instagram"></i>
+        </a>
+      </li>
+    </ul>
+    <div className="box-content">
+      <div className="box">
+        <h3>About</h3>
+        <ul>
+          <li>
+            <a href="#">How it works</a>
+          </li>
+          <li>
+            <a href="#">Why Us?</a>
+          </li>
+          <li>
+            <a href="#">Media</a>
+          </li>
+          <li>
+            <a href="#">Reviews</a>
+          </li>
+          <li>
+            <a href="#">Our stary</a>
+          </li>
+          <li>
+            <a href="#">Terms of Sale</a>
+          </li>
+          <li>
+            <a href="#">Dekra</a>
+          </li>
+        </ul>
+      </div>
+      <div className="box">
+        <h3>Buying & Selling</h3>
+        <ul>
+          <li>
+            <a href="#">Buy a car</a>
+          </li>
+          <li>
+            <a href="#">Sell a car</a>
+          </li>
+          <li>
+            <a href="#">Car auctions</a>
+          </li>
+        </ul>
+      </div>
+      <div className="box">
+        <h3>Value Added Service</h3>
+        <ul>
+          <li>
+            <a href="#">Car finance </a>
+          </li>
+          <li>
+            <a href="#">Car finance calculator </a>
+          </li>
+          <li>
+            <a href="#">Car insurance</a>
+          </li>
+          <li>
+            <a href="#">Car auctions </a>
+          </li>
+          <li>
+            <a href="#">Car trade-ins</a>
+          </li>
+          <li>
+            <a href="#">Dealers</a>
+          </li>
+        </ul>
+      </div>
+      <div className="box">
+        <h3>Most Popular Brands</h3>
+        <ul>
+          <li>
+            <a href="#">Volkswagen </a>
+          </li>
+          <li>
+            <a href="#">Toyota </a>
+          </li>
+          <li>
+            <a href="#">Ford</a>
+          </li>
+          <li>
+            <a href="#">BMW </a>
+          </li>
+          <li>
+            <a href="#">Hyundai</a>
+          </li>
+          <li>
+            <a href="#">Nissan</a>
+          </li>
+          <li>
+            <a href="#">Mercedes-Benz</a>
+          </li>
+          <li>
+            <a href="#">Audi</a>
+          </li>
+          <li>
+            <a href="#">Renault</a>
+          </li>
+          <li>
+            <a href="#">Chevrolet</a>
+          </li>
+        </ul>
+      </div>
+      <div className="box">
+        <h3>Get in Touch</h3>
+        <ul>
+          <li>
+            <a href="#">Customer care</a>
+          </li>
+          <li>
+            <a href="#">Our Location</a>
+          </li>
+          <li>
+            <a href="#">FAQ's</a>
+          </li>
+          <li>
+            <a href="#">Careers</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>;
+// End Footer
