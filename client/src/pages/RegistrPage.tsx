e3f90d88ee11a52ab5026c2859624284c2850fe2
Atmani Chouaib <atmanichouaib22@gmail.com>
2024-04-01 02:00:23 +0000

    user registration form and page created

---
+++ client/src/pages/RegistrPage.tsx
@@ -0,0 +1,11 @@
+import Registerform from "@/components/custom/Registerform";
+
+const RegistrPage = () => {
+  return (
+    <div>
+      <Registerform />
+    </div>
+  );
+};
+
+export default RegistrPage;
