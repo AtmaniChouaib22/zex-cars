6808db35dcd9b23b6fb7753ead15893c1db0bb89
abdelileh <abdelileh@gmail.com>
2024-04-03 12:20:42 +0200

    modified landing page  add contact-us page

--- client/src/Layout/landing.tsx
+++ client/src/Layout/landing.tsx
@@ -1,24 +1,107 @@
 // Start Landing
 <div className="banner">
-  <div className="overlay">
+  <div className="content">
+    <h1>Click your next</h1>
+    <p>Quick. Easy. Simple. Sell your car today with immediate payment.</p>
+    <div className="btn">
+      <a id="sell" href="#">
+        <span>SELL MY CAR</span>
+        <i className="fa fa-play spacer-ml-8"></i>
+      </a>
+      <a id="buy" href="#">
+        <span>BUY A CAR</span>
+        <i className="fa fa-play spacer-ml-8"></i>
+      </a>
+    </div>
+  </div>
+  <video className="video-bg" autoplay muted loop>
+    <source src="../assets/landing.mp4" type="video/mp4" />
+  </video>
+</div>;
+// End Landing
+// Start Contact-us
+<div className="contact">
+  <div className="container">
+    <div className="main-head">
+      <h2>Customer care</h2>
+      <p>We value your support and feedback.</p>
+    </div>
+    <div className="col">
+      <div className="pl">
+        <i className="fa-solid fa-person-circle-question"></i>
+      </div>
+      <div className="info">
+        <span className="head">
+          <strong>
+            To ensure our customers are given the best possible support,
+            WeBuyCars has implemented stringent measures to ensure total
+            customer satisfaction.
+          </strong>
+        </span>
+        <p>
+          Our customer care team exists to provide ongoing, excellent service
+          and operates:<strong>Mondays</strong> to
+          <strong>Fridays</strong>, from <strong>08:00</strong> to
+          <strong>17:00</strong>.
+        </p>
+      </div>
+    </div>
+    <hr />
+    <div className="main-head">
+      <h2>Talk to us</h2>
+      <p>Complete the relevant form below to get in touch.</p>
+    </div>
     <div className="content">
-      <h1>Click your next</h1>
-      <p>Quick. Easy. Simple. Sell your car today with immediate payment.</p>
-      <div>
-        <a id="sell" href="#">
-          <span>SELL MY CAR</span>
-          <i className="fa fa-play spacer-ml-8"></i>
-        </a>
-        <a id="buy" href="#">
-          <span>BUY A CAR</span>
-          <i className="fa fa-play spacer-ml-8"></i>
-        </a>
+      <form action="" method="get">
+        <input
+          className="main-input"
+          type="text"
+          name="name"
+          placeholder="your name"
+        />
+        <input
+          className="main-input"
+          type="email"
+          name="email"
+          placeholder="your email"
+        />
+        <textarea
+          className="main-input"
+          name="message"
+          placeholder="your message"></textarea>
+        <input type="submit" value="send message" />
+      </form>
+    </div>
+    <hr />
+    <div className="main-head">
+      <h2>Our Call Centres</h2>
+      <p>If you need anything else, please be in touch</p>
+    </div>
+    <div className="col">
+      <div className="pl">
+        <i className="fa-solid fa-user-clock"></i>
+      </div>
+      <div className="info">
+        <span className="head">
+          <p>
+            Customer care call centre: <strong>087 057 0000</strong>
+          </p>
+          <p>
+            Customer care email:
+            <strong>customercare@webuycars.co.za</strong>
+          </p>
+        </span>
+        <p>
+          Our customer care team exists to provide ongoing, excellent service
+          and operates:<strong>Mondays</strong> to
+          <strong>Fridays</strong>, from <strong>08:00</strong> to
+          <strong>17:00</strong>.
+        </p>
       </div>
     </div>
   </div>
 </div>;
-// End Landing
-
+// End Contact-us
 //  Start Services
 <div className="services" id="services">
   <div className="container">
