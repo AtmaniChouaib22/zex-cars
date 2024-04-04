4090ae748ff24f892c589b93400a7cc948bc7ba7
Atmani Chouaib <atmanichouaib22@gmail.com>
2024-04-04 23:38:08 +0000

    Add admin functionality and logout feature

--- client/src/lib/fetchUtils.ts
+++ client/src/lib/fetchUtils.ts
@@ -125,6 +125,114 @@ const buyCar = async (car_id: string, payment_method: string) => {
   );
   return response.data;
 };
+
+//logout
+
+const logout = async () => {
+  const response = await axios.post(
+    "http://localhost:3000/api/logout",
+    {},
+    {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return response.data;
+};
+
+// admin fetch waiting cars
+
+const fetchWaitingCars = async () => {
+  const response = await axios.get("http://localhost:3000/api/waitingcars", {
+    withCredentials: true,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return response.data;
+};
+
+//admin fetch pending deals
+
+const fetchUnconfirmedDeals = async () => {
+  const response = await axios.get(
+    "http://localhost:3000/api/unconfirmeddeals",
+    {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return response.data;
+};
+
+//admin set car to available
+
+const setCarToAvailable = async (id: string) => {
+  const response = await axios.patch(
+    "http://localhost:3000/api/setavailable",
+    { id },
+    {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return response.data;
+};
+
+//set car to rejected
+
+const setCarToRefused = async (id: string) => {
+  const response = await axios.patch(
+    "http://localhost:3000/api/setrejected",
+    { id },
+    {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return response.data;
+};
+
+//confirm deal
+
+const confirmDeal = async (id: string) => {
+  const response = await axios.patch(
+    "http://localhost:3000/api/confirmdeal",
+    { id },
+    {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return response.data;
+};
+
+//reject deal
+
+const rejectDeal = async (id: string) => {
+  const response = await axios.patch(
+    "http://localhost:3000/api/rejectdeal",
+    { id },
+    {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return response.data;
+};
+
 export {
   fetchAvailableCars,
   login,
@@ -134,4 +242,11 @@ export {
   fetchUserAvatar,
   fetchSingleCar,
   buyCar,
+  logout,
+  fetchWaitingCars,
+  fetchUnconfirmedDeals,
+  setCarToAvailable,
+  setCarToRefused,
+  confirmDeal,
+  rejectDeal,
 };
